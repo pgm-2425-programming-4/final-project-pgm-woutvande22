178537ef7834894e6ae5cb07d3c3685dcadacba7
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2025-05-18 13:07:39 +0200

    Refactor Backlog component structure and remove unused CSS styles

--- client/src/components/PaginatedBacklog/Backlog/Backlog.jsx
+++ client/src/components/PaginatedBacklog/Backlog/Backlog.jsx
@@ -1,9 +1,11 @@
 export function Backlog({ backlog }) {
   return (
-    <ul>
-      {backlog.map((item) => (
-        <li key={item.id}>{item.title}</li>
-      ))}
-    </ul>
+    <div>
+      <ul>
+        {backlog.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
