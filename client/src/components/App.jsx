706ab0dd2d2d7eb048dad97364af16bfb41c9738
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2025-05-20 11:01:45 +0200

    fixed import casing for Backlog and Pagination components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import { PaginatedBacklog } from "./paginatedbacklog/backlog-list.jsx";
-import { Backlog } from "./backlog/backlog.jsx";
-import { Pagination } from "./pagination/pagination.jsx";
 
 const queryClient = new QueryClient();
 
--- client/src/components/PaginatedBacklog/backlog-list.jsx
+++ client/src/components/PaginatedBacklog/backlog-list.jsx
@@ -1,59 +1,59 @@
 import { useEffect, useState } from "react";
 import { fetchBacklog } from "../../data/fetchBacklog.js";
-import { Backlog } from "./Backlog/Backlog.jsx";
+import { Backlog } from "./backlog/backlog.jsx";
 import { Pagination } from "./pagination/pagination.jsx";
 import { useQuery } from "@tanstack/react-query";
 const PAGE_SIZE_OPTIONS = [5, 10, 20, 50];
 
 export function PaginatedBacklog() {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pageCount, setPageCount] = useState(1);
-    const [pageSize, setPageSize] = useState(PAGE_SIZE_OPTIONS[0]);
-    const [backlog, setBacklog] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageCount, setPageCount] = useState(1);
+  const [pageSize, setPageSize] = useState(PAGE_SIZE_OPTIONS[0]);
+  const [backlog, setBacklog] = useState([]);
 
-    function handlePageChanged(pageNumber) {
-      setCurrentPage(pageNumber);
-    }
+  function handlePageChanged(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
 
-    function handlePageSizeChanged(size) {
-      setPageSize(size);
-    }
+  function handlePageSizeChanged(size) {
+    setPageSize(size);
+  }
 
-    const { isPending, isError, error, data } = useQuery({
-      queryKey: ["todos", { currentPage, pageSize }],
-      queryFn: () => fetchBacklog(currentPage, pageSize),
-    });
+  const { isPending, isError, error, data } = useQuery({
+    queryKey: ["todos", { currentPage, pageSize }],
+    queryFn: () => fetchBacklog(currentPage, pageSize),
+  });
 
-    useEffect(() => {
-      if (data) {
-        if (currentPage > data.meta.pagination.pageCount) {
-          setCurrentPage(data.meta.pagination.pageCount);
-        }
-        setBacklog(data.data);
-        setPageCount(data.meta.pagination.pageCount);
+  useEffect(() => {
+    if (data) {
+      if (currentPage > data.meta.pagination.pageCount) {
+        setCurrentPage(data.meta.pagination.pageCount);
       }
-    }, [data, currentPage]);
-
-    if (isPending) {
-      return <span>Loading...</span>;
+      setBacklog(data.data);
+      setPageCount(data.meta.pagination.pageCount);
     }
+  }, [data, currentPage]);
 
-    if (isError) {
-      return <span>Error: {error.message}</span>;
-    }
+  if (isPending) {
+    return <span>Loading...</span>;
+  }
 
-    return (
-      <>
-        <div style={{ marginBottom: "2rem" }}>
-          <Backlog backlog={backlog}/>
-        </div>
-        <Pagination
-          currentPage={currentPage}
-          pageCount={pageCount}
-          pageSize={pageSize}
-          onPageChanged={handlePageChanged}
-          onPageSizeChanged={handlePageSizeChanged}
-        />
-      </>
-    );
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <>
+      <div style={{ marginBottom: "2rem" }}>
+        <Backlog backlog={backlog} />
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        pageCount={pageCount}
+        pageSize={pageSize}
+        onPageChanged={handlePageChanged}
+        onPageSizeChanged={handlePageSizeChanged}
+      />
+    </>
+  );
 }
