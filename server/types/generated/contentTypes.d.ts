cf4683eaf353645fb9ce4cb4da9575f316fc83c0
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2025-06-11 13:53:42 +0200

    added description to tasks

--- server/types/generated/contentTypes.d.ts
+++ server/types/generated/contentTypes.d.ts
@@ -447,6 +447,8 @@ export interface ApiTaskTask extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    description: Schema.Attribute.Text &
+      Schema.Attribute.DefaultTo<'Beschrijving van de taak'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::task.task'> &
       Schema.Attribute.Private;
