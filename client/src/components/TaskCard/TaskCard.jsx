cddbfd0367776125f112708dffc5ae63f17fcf80
pgm-woutvande22 <wout.vanderbauwhede@student.arteveldehs.be>
2025-06-11 14:04:19 +0200

    updated task-card look

--- client/src/components/TaskCard/TaskCard.jsx
+++ client/src/components/TaskCard/TaskCard.jsx
@@ -3,21 +3,29 @@ import { Tag } from "./Tag/tag";
 
 export function TaskCard({ title, tasks, emptyText }) {
   return (
-    <div className='card has-background-primary has-text-primary-00'>
-      <h2 className='is-size-3'>{title}</h2>
-      <div>
-        {tasks.length === 0
-          ? <span>{emptyText}</span>
-          : tasks.map((task) => (
-              <div key={task.id}>
-                {task.title}
-                <div>
-                  {task.tags?.map((tag) => (
-                    <Tag key={tag.id} title={tag.title} />
-                  ))}
+    <div className="mb-5">
+      <h2 className="title is-3">{title}</h2>
+      <div className="columns is-multiline">
+        {tasks.length === 0 ? (
+          <div className="column is-12">
+            <div className="notification is-light">{emptyText}</div>
+          </div>
+        ) : (
+          tasks.map((task) => (
+            <div className="column is-4" key={task.id}>
+              <div className="card">
+                <div className="card-content">
+                  <p className="title is-5">{task.title}</p>
+                  <div className="tags">
+                    {task.tags?.map((tag) => (
+                      <Tag key={tag.id} title={tag.title} />
+                    ))}
+                  </div>
                 </div>
               </div>
-            ))}
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
